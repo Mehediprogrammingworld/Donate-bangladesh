3c3b53554367167eadc2f3552d8bb5d1758fcc02
Mehediprogrammingworld <mehedihassansweetheartex@gmail.com>
2024-09-24 00:08:01 +0600

    added an index

---
+++ index.js
@@ -0,0 +1,6 @@
+document.getElementById('donate').addEventListener('click', function(){
+       
+    const moneyAdd = document.getElementById('add_balance').innerText;
+    const rebens = document.getElementById('rebens-balance').innerText;
+    console.log( moneyAdd, rebens);
+});
